5bd7904cef5a2a1fb99abeef5e15132526d48315
Craitson <craitson@gmail.com>
2017-12-17 19:03:27 -0200

    Atualizar Versao do projeto

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,11 @@
+import java.util.logging.Logger;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Ok");
-        System.out.println("Ok");
+        System.out.println("Hello");
+        Logger.getGlobal().info("Hello");
     }
 
 }
--- src/main/java/WordProblemSolver.java
+++ src/main/java/WordProblemSolver.java
@@ -1,3 +1,6 @@
+
+
+//XXXX Renomear classe
 public class WordProblemSolver {
     public int solve(String s) {
         return 2;
